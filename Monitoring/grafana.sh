8504fb12a7aa09357aec26d90f29d5648448d814
Yohankristian <forjooxonly0@gmail.com>
2021-08-27 15:30:16 +0700

    New update

--- Monitoring/grafana.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 3000:3000 -v /media/master/Data/\[Project\]\ Blibli/grafana_db:/var/lib/grafana grafana/grafana
--- Monitoring/monitoring.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 9090:9090 -v /media/master/Data/\[Project\]\ Blibli/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
